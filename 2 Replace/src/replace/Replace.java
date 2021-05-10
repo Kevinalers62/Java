aa2efea49f4cd128bc36dd00cc656fb313df9c36
Kevin Phillip Alers <56022682+Kevinalers62@users.noreply.github.com>
2021-05-10 08:02:29 +0200

    Add files via upload

---
+++ 2 Replace/src/replace/Replace.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package replace;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Replace 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        int First = 7;
+        int second = 19;
+        
+        System.out.println("First = " + First + " Second = " + second);
+    }
+    
+}
